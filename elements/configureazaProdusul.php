52b60438fb167286a6ca07a644682c5088d28cff
Deiu <d3iu25@gmail.com>
2023-10-23 17:25:58 +0300

    config design 2

--- elements/configureazaProdusul.php
+++ elements/configureazaProdusul.php
@@ -4,52 +4,9 @@
             <h4 class="config-prod-h3 mt-2">Configureaza productul</h4>
             <hr class="top-hr">
             <div class="dropdown">
-                <button class="dropdown-button-product dropdown-toggle dropdown-btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="dropdown-button-product d-flex justify-content-between" type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-expanded="false">
                     Profil
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
-                </ul>
-            </div>
-            <hr class="custom-hr">
-            <div class="dropdown">
-                <button class="dropdown-button-product dropdown-toggle dropdown-btn" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                    Culoare
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
-                </ul>
-            </div>
-            <hr class="custom-hr">
-            <div class="dropdown">
-                <button class="dropdown-button-product dropdown-toggle dropdown-btn" type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-expanded="false">
-                    Sticla
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton3">
-                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
-                </ul>
-            </div>
-            <hr class="custom-hr">
-            <div class="dropdown">
-                <button class="dropdown-button-product dropdown-toggle dropdown-btn" type="button" id="dropdownMenuButton4" data-bs-toggle="dropdown" aria-expanded="false">
-                    Latime
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton4">
-                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
-                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
-                </ul>
-            </div>
-            <hr class="custom-hr">
-            <div class="dropdown">
-                <button class="dropdown-button-product dropdown-toggle dropdown-btn" type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-expanded="false">
-                    Inaltime
+                    <div><span class="dropdown-arrow"></span></div>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
                     <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
@@ -59,7 +16,55 @@
             </div>
             <hr class="custom-hr">
             <div class="dropdown">
-                <button class="dropdown-button-product" type="button" id="num-of-items" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="dropdown-button-product d-flex justify-content-between" type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-expanded="false">
+                    Culoare
+                    <div><span class="dropdown-arrow"></span></div>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
+                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
+                </ul>
+            </div>
+            <hr class="custom-hr">
+            <div class="dropdown">
+                <button class="dropdown-button-product d-flex justify-content-between" type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sticla
+                    <div><span class="dropdown-arrow"></span></div>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
+                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
+                </ul>
+            </div>
+            <hr class="custom-hr">
+            <div class="dropdown">
+                <button class="dropdown-button-product d-flex justify-content-between" type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-expanded="false">
+                    latime
+                    <div><span class="dropdown-arrow"></span></div>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
+                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
+                </ul>
+            </div>
+            <hr class="custom-hr">
+            <div class="dropdown">
+                <button class="dropdown-button-product d-flex justify-content-between" type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-expanded="false">
+                    Inaltime
+                    <div><span class="dropdown-arrow"></span></div>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
+                    <li><a class="dropdown-item" href="#">Opțiune 1</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 2</a></li>
+                    <li><a class="dropdown-item" href="#">Opțiune 3</a></li>
+                </ul>
+            </div>
+            <hr class="custom-hr">
+            <div class="dropdown">
+                <button class="dropdown-button-product d-flex" type="button" id="num-of-items" data-bs-toggle="dropdown" aria-expanded="false">
                     Bucati
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton6">
@@ -72,9 +77,9 @@
     </div>
     <div class="item-config-footer">
         <div class="d-flex flex-row">
-                <button class="button-config">-</button>
-                <button class="item-config-number">2</button>
-                <button class="button-config">+</button>
+            <button class="button-config">-</button>
+            <button class="item-config-number">2</button>
+            <button class="button-config">+</button>
         </div>
     </div>
 </div>
