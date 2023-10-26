19cde8e4f722075c49bde0aecf65b2fabdf07230
Deiu <d3iu25@gmail.com>
2023-10-26 14:03:26 +0300

    cart responsive

--- elements/orderSummary.php
+++ elements/orderSummary.php
@@ -1,20 +1,39 @@
-<div class="sumary-container">
-        <h3 class="styled-header">Sumar Comanda</h3>
-        <div class="row">
-            <div class="col-sm-6 left-sumary">
-                <p>1x Usa intrare 1 canat Dreapta</p>
-                <p>1 x Usa intrare 1 canat Stanga</p>
-                <p>Pret fara TVA</p>
-                <p>Suprafata</p>
-                <p>Pret cu TVA</p>
-            </div>
-            <div class="col-sm-6 right-sumary text-end">
-                <p>1214.81</p>
-                <p>1214.81</p>
-                <p>2429.62</p>
-                <p>2.7</p>
-                <p>2891.25</p>
-            </div>
+<div class="sumary-container big-sumary">
+    <h3 class="styled-header">Sumar Comanda</h3>
+    <div class="row">
+        <div class="col-sm-6 left-sumary">
+            <p>1x Usa intrare 1 canat Dreapta</p>
+            <p>1 x Usa intrare 1 canat Stanga</p>
+            <p>Pret fara TVA</p>
+            <p>Suprafata</p>
+            <p>Pret cu TVA</p>
         </div>
-        <button class="btn btn-primary cart-button">Plaseaza Comanda</button>
+        <div class="col-sm-6 right-sumary text-end">
+            <p>1214.81</p>
+            <p>1214.81</p>
+            <p>2429.62</p>
+            <p>2.7</p>
+            <p>2891.25</p>
+        </div>
     </div>
+    <button class="btn btn-primary cart-button">Plaseaza Comanda</button>
+</div>
+
+<div class="sumary-container small-sumary">
+    <h3 class="styled-header-small">Sumar Comanda</h3>
+    <div class="row">
+        <div class="sumary-name">
+            <p>1x Usa intrare 1 canat Dreapta</p>
+            <p>1214.81 <hr></p>
+            <p>1 x Usa intrare 1 canat Stanga</p>
+            <p>1214.81<hr></p>
+            <p>Pret fara TVA</p>
+            <p>2429.62<hr></p>
+            <p>Suprafata</p>
+            <p>2.7<hr></p>
+            <p>Pret cu TVA</p>
+            <p>2891.25</p>
+        </div>
+    </div>
+    <button class="btn btn-primary cart-button">Plaseaza Comanda</button>
+</div>
