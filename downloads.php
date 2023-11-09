afe5fe389a24eb6de270919d9f19da32407364f0
Deiu <d3iu25@gmail.com>
2023-11-09 14:28:29 +0200

    download page

--- downloads.php
+++ downloads.php
@@ -20,40 +20,71 @@
     </div>
 </section>
 
-<section class="mb-5">
-    <div class="row p-4 justify-content-center g-4 bottom">
-        <div class="col-md-3 border border-3 rounded p-4 mr-3 download-card">
-            <h2>Sticlă izolatoare</h2>
-            <p>Umplerea cu gaz argon în spațiul dintre geamuri aduce o contribuție suplimentară la creșterea izolației termice. Cu un geam triplu foarte izolant de la Aplast, puteți atinge chiar valori Uw de până la 0,68 W / m²K și astfel vă puteți multiplica potențialul de economisire.</p>
-        </div>
-        <div class="col-md-3 border border-3 rounded p-4 mx-3 download-card">
-            <h2>Distanțier de geam</h2>
-            <p>Acest lucru menține gazul Argon izolant termic în spațiul dintre geamuri și previne pătrunderea vaporilor de apă. Distanțierele de sticlă din PVC foarte izolante reduc aburirea panoului de pe partea camerei, la marginile baghetelor de fixare ale geamului.</p>
-        </div>
-        <div class="col-md-3 border border-3 rounded p-4 ml-3 download-card">
-            <h2>Profilul ferestrei</h2>
-            <p>Relația dintre numărul de camere ale profilului, grosimea profilului și adâncimea de instalare a acestuia trebuie să fie optimă. De exemplu, un profil de 70 mm ar trebui să aibă cel puțin 5 camere de profil pentru o izolare optimă a cadrului.</p>
-        </div>
-    </div>
-</section>
-
 <?php
 $sectiuni = [
     [
-        'titlu' => 'Sticlă izolatoare',
-        'text' => 'Umplerea cu gaz argon în spațiul dintre geamuri aduce o contribuție suplimentară la creșterea izolației termice. Cu un geam triplu foarte izolant de la Aplast, puteți atinge chiar valori Uw de până la 0,68 W / m²K și astfel vă puteți multiplica potențialul de economisire.'
+        'img' => './download/visio-robusto-slim.webp',
+        'pdf' => './download/GE_oki-Catalog-DE-email-FINAL.pdf',
+        'titlu' => 'Vision Robusto Slim & Aluminium - versiune în limba germană'
     ],
-    // Adaugă aici mai multe înregistrări...
+    [
+        'img' => './download/visio-robusto-slim.webp',
+        'pdf' => './download/Catalog-Engleza-2021-Veka-SL70-si-Aplast-70-Plus-LowRes-Apr-2021.pdf',
+        'titlu' => 'Vision Robusto Slim & Aluminium - versiune în limba engleză'
+    ],
+    [
+        'img' => './download/visio-robusto-slim.webp',
+        'pdf' => './download/Catalog-Romania-2021-Veka-SL70-si-Aplast-70-Plus-LowRes-Apr-2021.pdf',
+        'titlu' => 'Vision Robusto Slim & Aluminiu - versiune în limba romană'
+    ],
+    [
+        'img' => './download/Catalog-Franta-2019-Aplast-70-Plus.webp',
+        'pdf' => './download/Catalog-Franta-2019-Aplast-70-Plus.pdf',
+        'titlu' => 'Vision 70 Plus - versiune în limba franceză'
+    ],
+    [
+        'img' => './download/Screenshot-2021-05-26-at-21.00.57.webp',
+        'pdf' => './download/oki-Catalog-IT-email-FINAL.pdf',
+        'titlu' => 'Aplast 70 Plus & Veka Softline - versiune în limba italiană'
+    ],
+    [
+        'img' => './download/Catalog-Franta-2019-Veka-Softline-70.webp',
+        'pdf' => './download/Catalog-Franta-2019-Veka-Softline-70.pdf',
+        'titlu' => 'Veka Softline 70 - versiune în limba franceză'
+    ],
+    [
+        'img' => './download/cer1.webp',
+        'pdf' => './download/Certificat-de-conformitate-nr.-726-Ferestre-si-usi_0001-5.pdf',
+        'titlu' => 'Certificat de conformitate nr. 726 - Ferestre si usi'
+    ],
+    [
+        'img' => './download/cer3.webp',
+        'pdf' => './download/ISO-7.pdf',
+        'titlu' => 'Certificat ISO'
+    ],
+    [
+        'img' => './download/cer2.webp',
+        'pdf' => './download/Certificat-de-conformitate-nr.725-Elemente-de-vitraj-izolante_0001-4.pdf',
+        'titlu' => 'Certificat de conformitate nr.725 - Elemente de vitraj izolante'
+    ],
 ];
 
-echo '<section class="mb-5"><div class="row p-4 justify-content-center g-4 bottom">';
+echo '<section class="mb-5 container"><div class="row p-4 justify-content-center g-4 gap-4 bottom">';
 
 foreach ($sectiuni as $sectiune) {
     echo '<div class="col-md-3 border border-3 rounded p-4 mr-3 download-card">';
-    echo '<h2>' . $sectiune['titlu'] . '</h2>';
-    echo '<p>' . $sectiune['text'] . '</p>';
+    echo '<a href="' . $sectiune['pdf'] . '" target="_blank">';
+    echo '<img src="' . $sectiune['img'] . '" class="img-fluid" alt="img">';
+    echo '<h6 class="text-center mt-1">' . $sectiune['titlu'] . '</h6>';
+    echo '</a>';
     echo '</div>';
+
 }
 
 echo '</div></section>';
 ?>
+
+<?php include "includes/footer.php"; ?>
+<!-- 
+5205914501642
+160.14 -->
