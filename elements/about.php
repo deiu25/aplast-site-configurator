78ec5ab2787648282db7b9a025eeea3cb335a19b
Deiu <d3iu25@gmail.com>
2023-11-13 16:26:32 +0200

    about section responsive

--- elements/about.php
+++ elements/about.php
@@ -6,13 +6,15 @@
     </div>
     <div class="mt-5"></div>
     <h1 class="title mb-5">Despre Noi</h1>
-    <div class="container d-flex flex-row">
-        <div class="pctDeVanzare aboutLeft">
-            <h1 class="aboutSubtitle">Orientare către client</h1>
-            <p class="text-center fs-5 fw-bold text-muted">Cu prezență internațională din 2002, suntem un reputat producător de uși și ferestre, specializat în furnizarea către lanturi exigente de bricolaj cu amănuntul în Europa. Oferim clienților noștri soluții inteligente în materie de calitate, logistică și costuri. Ne străduim să oferim servicii imbatabile pentru clienți</p>
-        </div>
-        <div>
-            <img class="img-fluid aboutImage" src="./images/aboutImage.webp" alt="aboutImage">
+    <div class="container">
+        <div class="row gx-5">
+            <div class="col-lg-6 order-lg-2">
+                <img class="img-fluid aboutImage" src="./images/aboutImage.webp" alt="aboutImage">
+            </div>
+            <div class="col-lg-6 order-lg-1 pctDeVanzare aboutLeft">
+                <h1 class="aboutSubtitle">Orientare către client</h1>
+                <p class="text-center fs-5 fw-bold text-muted ">Cu prezență internațională din 2002, suntem un reputat producător de uși și ferestre, specializat în furnizarea către lanturi exigente de bricolaj cu amănuntul în Europa. Oferim clienților noștri soluții inteligente în materie de calitate, logistică și costuri. Ne străduim să oferim servicii imbatabile pentru clienți</p>
+            </div>
         </div>
     </div>
 </section>
