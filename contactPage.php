df00379ab1fd8f53c3db88378f49431069e5db1c
Deiu <d3iu25@gmail.com>
2023-11-14 15:48:27 +0200

    contact page  responsive

--- contactPage.php
+++ contactPage.php
@@ -3,47 +3,7 @@
 <!-- Navigation -->
 <?php include "includes/navigation.php"; ?>
 
-<section class="position-relative">
-    <img class="img-fluid" src="getsvg.php?svg=headerContact" alt="headerContact">
-
-    <h1 class="h1-contact-page-h left position-absolute">Birouri</h1>
-    <img class="img-fluid logo-contact-page right position-absolute" src="getsvg.php?svg=logoWhite" alt="logo">
-
-    <div class="poligon-div d-flex justify-content-center position-absolute bottom-0 start-50 translate-middle-x">
-        <div class="poligon d-flex flex-row pctDeVanzare">
-            <div class="mx-3 mt-4">
-                <img class="" src="getsvg.php?svg=contactPhone" alt="contactPhone">
-            </div>
-            <div class="mt-4 me-5">
-                <h5 class="text-center heroh5">Numar De Telefon</h5>
-                <p class="text-center heroP">+40 751 103 377</p>
-            </div>
-        </div>
-        <div class="poligon d-flex flex-row pctDeVanzare">
-            <div class="mx-3 mt-4">
-                <img class="" src="getsvg.php?svg=contactOfices" alt="contactOfices">
-            </div>
-            <div class="mt-4 me-5">
-                <h5 class="text-center heroh5">Prahova, Romania</h5>
-                <p class="text-center heroP">Com. Ceptura</p>
-            </div>
-        </div>
-        <div class="poligon d-flex flex-row pctDeVanzare">
-            <div class="mx-3 mt-4">
-                <img class="" src="getsvg.php?svg=contactEmail" alt="contactEmail">
-            </div>
-            <div class="mt-4 me-5">
-                <h5 class="text-center heroh5">Email</h5>
-                <p class="text-center heroP">office@aplast.com</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="bgColor">
-        <div></div>
-        <h1 class="abouth1">to create space</h1>
-    </div>
-</section>
+<?php include "elements/contactPageHero.php"; ?>
 
 <section>
     <div class="arrow-container mt-5">
---
+++ elements/contactPageHero.php
@@ -0,0 +1,75 @@
+<section class="position-relative">
+    <img class="img-fluid" src="getsvg.php?svg=headerContact" alt="headerContact">
+    <h1 class="h1-contact-page-h left position-absolute">Contact</h1>
+    <img class="img-fluid logo-contact-page right position-absolute" src="getsvg.php?svg=logoWhite" alt="logo">
+    </div>
+    <div class="div-relative">
+        <div class="poligon-div d-flex justify-content-center position-absolute bottom-0 start-50 translate-middle-x">
+            <div class="poligon d-flex flex-row pctDeVanzare">
+                <div class="mx-3 mt-4">
+                    <img class="" src="getsvg.php?svg=contactPhone" alt="contactPhone">
+                </div>
+                <div class="mt-4 me-5">
+                <h5 class="text-center heroh5">Numar De Telefon</h5>
+                <p class="text-center heroP">+40 751 103 377</p>
+            </div>
+            </div>
+            <div class="poligon d-flex flex-row pctDeVanzare">
+                <div class="mx-3 mt-4">
+                    <img class="" src="getsvg.php?svg=contactOfices" alt="contactOfices">
+                </div>
+                <div class="mt-4 me-5">
+                <h5 class="text-center heroh5">Prahova, Romania</h5>
+                <p class="text-center heroP">Com. Ceptura</p>
+            </div>
+            </div>
+            <div class="poligon d-flex flex-row pctDeVanzare">
+                <div class="mx-3 mt-4">
+                    <img class="" src="getsvg.php?svg=contactEmail" alt="contactEmail">
+                </div>
+                <div class="mt-4 me-5">
+                <h5 class="text-center heroh5">Email</h5>
+                <p class="text-center heroP">office@aplast.com</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="bgColor">
+        <div></div>
+        <h1 class="abouth1">sometexthere</h1>
+    </div>
+</section>
+
+<section class="container cont-relative-small-show">
+    <div class="d-flex justify-content-center">
+        <div class="poligon-div justify-content-center mt-5">
+            <div class="poligon-small">
+                <div class="top d-flex flex-column justify-content-end align-items-center">
+                    <img class="mb-3" src="getsvg.php?svg=contactPhone" alt="contactPhone">
+                    <div class="text-center">
+                        <h5 class="heroh5">Numar De Telefon</h5>
+                        <p class="heroP">+40 751 103 377</p>
+                    </div>
+                </div>
+            </div>
+            <div class="poligon-small">
+                <div class="top d-flex flex-column justify-content-end align-items-center">
+                <img class="mb-3" src="getsvg.php?svg=contactOfices" alt="contactOfices">
+                    <div class="text-center">
+                        <h5 class="heroh5">Prahova, Romania</h5>
+                        <p class="heroP">Com. Ceptura</p>
+                    </div>
+                </div>
+            </div>
+            <div class="poligon-small">
+                <div class="top d-flex flex-column justify-content-end align-items-center">
+                <img class="mb-3" src="getsvg.php?svg=contactEmail" alt="contactEmail">
+                    <div class="text-center">
+                        <h5 class="heroh5">Email</h5>
+                        <p class="heroP">office@aplast.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
