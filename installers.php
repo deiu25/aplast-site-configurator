9200d0b684b7f35a6c2ae6fc0af30d94d218637d
Deiu <d3iu25@gmail.com>
2023-11-15 13:19:36 +0200

    installers responsive

--- installers.php
+++ installers.php
@@ -15,29 +15,31 @@
     </div>
 </section>
 
-<section class="production-container">
-    <div class="arrow-container">
+<section class="lines-production-container">
+    <div class="arrow-container mb-5">
         <div class="oval-container">
             <i class="bi bi-arrow-down arrow-icon"></i>
         </div>
     </div>
-    <div class="container d-flex flex-row mt-5">
-        <div class="left production-left">
-            <h1 class="production-subtitle">Partenerul tău din 2002</h1>
-            <p class="production-p fs-5 fw-bold text-muted">Echipa Aplast este formată din experți din industrie cu mai mult de 15 ani de experiență. Ei cunosc provocările cu care te confrunți și găsesc modalități de a le aborda.</p>
+    <div class="container">
+        <div class="row gx-5">
+            <div class="col-lg-6 order-lg-2">
+                <img class="img-fluid aboutImage" src="./images/installer.webp" alt="installer">
+            </div>
+            <div class="col-lg-6 order-lg-1 pctDeVanzare aboutLeft">
+                <h1 class="production-subtitle">Partenerul tău din 2002</h1>
+                <p class="production-p fs-5 fw-bold text-muted">Echipa Aplast este formată din experți din industrie cu mai mult de 15 ani de experiență. Ei cunosc provocările cu care te confrunți și găsesc modalități de a le aborda.</p>
             <p class="production-p fs-5 fw-bold text-muted">Știm cât de important este să ne susținem partenerii și că acest lucru ne poate dezvolta afacerea iar asta se bazează pe satisfacția clienților. Un client mulțumit va continua să lucreze cu noi și ne va recomanda în continuare.</p>
             <p class="production-p fs-5 fw-bold text-muted">Sarcina noastră este de a vinde un pachet complet în jurul produselor noastre prin consultanță specializată, livrări fiabile și servicii post-vânzare orientate către client. Suntem gata să împărtășim cunoștințele noastre pentru a vă ajuta să vă dezvoltați propria afacere.</p>
-        </div>
-        <div class="right mt-5">
-            <img class="img-fluid production-image" src="./images/installer.webp" alt="installer">
+            </div>
         </div>
     </div>
 </section>
 
-<section class="production-mid mt-5">
+<section class="production-mid mt-5 mb-5">
     <div class="prod-img-container">
-        <img class="img-fluid prod-img-m" src="./images/instalatori_banner_mid.webp" alt="production">
-        <h1 class="h1-prod-m bottom">Ne mândrim cu experiență de piață începând din 2002, timp în care am investit în producție eficientă, calitate și oameni bine pregătiți</h1>
+        <img class="img-fluid inst-img-m" src="./images/instalatori_banner_mid.webp" alt="production">
+        <h1 class="h1-inst-m bottom">Ne mândrim cu experiență de piață începând din 2002, timp în care am investit în producție eficientă, calitate și oameni bine pregătiți</h1>
     </div>
 </section>
 
@@ -45,7 +47,7 @@
     <div class="text-center">
         <h1 class="production-subtitle mb-4">Contact</h1>
         <p class="fs-5 fw-bold text-muted mt-3">Luați legătura cu noi și deveniți partener. Indiferent dacă sunteți reprezentantul unui lanț de magazine de bricolaj de renume, un instalator cu experiență, un inginer civil sau un arhitect, suntem aici pentru a vă sprijini în maximizarea potențialului afacerii dvs. Prin colaborarea cu noi, puteți exploata la maximum oportunitățile și resursele disponibile</p>
-        <a href="contactPage.php"><button class="btn btn-outline-primary mt-2">Contactează-ne</button></a>
+        <a href="contactPage.php"><button class="btn btn-outline-primary mt-2 mb-5">Contactează-ne</button></a>
     </div>
 </section>
 
